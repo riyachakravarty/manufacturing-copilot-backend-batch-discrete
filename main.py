f7185f6f3e6e1028a61e388fe0a32da5dd526eff
riyachakravarty <riya.chakravarty2010@gmail.com>
2025-07-02 20:46:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -145,15 +145,15 @@ def visualize_missing_data(input_text):
             )
 
         #Highlight Missing Timestamps (Datetime Gaps)
-        print("[DEBUG] Calculating missing datetime intervals...")
-        missing_dt_intervals = get_missing_datetime_intervals(uploaded_df, 'Date_time')
-        print(f"[DEBUG] Found {len(missing_dt_intervals)} missing datetime intervals.")
-        for start, end in missing_dt_intervals:
-            fig.add_vrect(
-                x0=start, x1=end,
-                fillcolor="red", opacity=0.2, line_width=0,
-                annotation_text="Missing datetime", annotation_position="top right"
-            )
+        #print("[DEBUG] Calculating missing datetime intervals...")
+        #missing_dt_intervals = get_missing_datetime_intervals(uploaded_df, 'Date_time')
+        #print(f"[DEBUG] Found {len(missing_dt_intervals)} missing datetime intervals.")
+        #for start, end in missing_dt_intervals:
+         #   fig.add_vrect(
+          #      x0=start, x1=end,
+           #     fillcolor="red", opacity=0.2, line_width=0,
+            #    annotation_text="Missing datetime", annotation_position="top right"
+            #)
 
         fig.update_layout(
             title=f"Missing Data Visualization: '{selected_variable}' Over Time",
