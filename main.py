dbbb6a25079e2309e0d6dfed2a5794275a6f619e
riyachakravarty <riya.chakravarty2010@gmail.com>
2025-09-09 16:08:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -631,7 +631,8 @@ def dualaxes_boxplot(req: DualBoxPlotRequest):
             print("   y length:", len(trace.get("y", [])))
         print("=== End of Debug ===")
     
-        return JSONResponse(content={"type": "plot", "data": json.loads(fig.to_json())})
+        #return JSONResponse(content={"type": "plot", "data": json.loads(fig.to_json())})
+        return JSONResponse(content={"type": "plot", "data": json.loads(result)})
 
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
